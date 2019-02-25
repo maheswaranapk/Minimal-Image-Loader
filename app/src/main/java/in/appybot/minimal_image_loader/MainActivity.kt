08668028d9f07c2106fc194bd7ab3f3104ed09b6
Mahes Sivakumar <maheswaranapk@gmail.com>
2019-02-25 16:19:54 +0530

    Initial Commit

---
+++ app/src/main/java/in/appybot/minimal_image_loader/MainActivity.kt
@@ -0,0 +1,12 @@
+package `in`.appybot.minimal_image_loader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
